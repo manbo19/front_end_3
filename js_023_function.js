ef9075aa9a87d422ea008b25dc220277164ed95d
jeongmin <jeongmin0619@gmail.com>
2022-09-21 18:12:06 +0900

    20220921

--- js_023_function.js
+++ js_023_function.js
@@ -32,18 +32,48 @@ pointPlus(3000);
 */
 
 //배열의 길이 리턴
-function length(data) {
+// function length(data) {
+//   return data.length;
+// }
+
+let length = (data) => {
   return data.length;
-}
+};
 
 //배열에서 특정 인덱스의 요소 리턴
-function charAt(data, index) {
+// function charAt(data, index) {
+//   return data[index];
+// }
+
+let charAt = (data, index) => {
   return data[index];
-}
+};
+
+//배열에서 대문자 개수를 리턴
+// function upperCnt(data) {
+//   let cnt = 0;
+//   for (let i = 0; i < data.length; i++) {
+//     if (data[i] >= "A" && data[i] <= "Z") {
+//       cnt++;
+//     }
+//   }
+//   return cnt;
+// }
+
+let upperCnt = (data) => {
+  let cnt = 0;
+  for (let i = 0; i < data.length; i++) {
+    if (data[i] >= "A" && data[i] <= "Z") {
+      cnt++;
+    }
+  }
+  return cnt;
+};
 
 let arr = ["j", "a", "v", "A"];
 console.log(`요소의 길이: ${length(arr)}`);
 console.log(`2인덱스 요소값 가져오기: ${charAt(arr, 2)}`);
+console.log(`대문자 개수:${upperCnt(arr)}`);
 
 // function length(arr) {
 //   console.log(`요소의 길이: ${arr.length}`);
