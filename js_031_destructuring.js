6ddd8089443ae75b8f820b1e29a6f234c6118772
jeongmin <jeongmin0619@gmail.com>
2022-09-23 17:15:01 +0900

    20220923

--- js_031_destructuring.js
+++ js_031_destructuring.js
@@ -78,3 +78,47 @@ console.log(`s=${s} r=${r}`); //s=1 r=2
 
 ////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////
+//completed의 값이 true인 데이터만 검색하라.
+let habits = [
+  { id: 1, content: "수영", completed: false },
+  { id: 2, content: "걷기", completed: true },
+  { id: 3, content: "명상", completed: true },
+  { id: 4, content: "독서", completed: false },
+];
+
+let listHabit = [];
+for (let i = 0; i < habits.length; i++) {
+  if (habits[i].completed) {
+    listHabit.push(habits[i]);
+  }
+}
+
+console.log(listHabit);
+//////////////////////////////////////////////
+let completedHabit = habits.filter((element) => {
+  return element.completed;
+});
+
+console.log(completedHabit);
+
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+//member변수에서 데이터를 추출하여 '고수 고객님은 서울에 거주합니다.'라고 출력하세요.
+
+let member = {
+  hname: "고수",
+  address: {
+    zipCode: "526-143",
+    city: "서울",
+  },
+};
+//////////////
+let { hname, address } = member;
+let { city } = address;
+console.log(`${hname} 고객님은 ${city}에 거주합니다.`);
+//////////////
+({
+  hname,
+  address: { city },
+} = member);
+console.log(`${hname} 고객님은 ${city}에 거주합니다.`);
