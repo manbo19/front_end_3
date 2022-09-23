a05d549344d28cbbbf458eebe08f773a583f03fe
jeongmin <jeongmin0619@gmail.com>
2022-09-23 18:17:53 +0900

    20220923

--- js_033_문제풀이.js
+++ js_033_문제풀이.js
@@ -10,35 +10,11 @@ let arr = [1, 2, 3, undefined, 4, undefined, undefined, 5];
 합계: 15
 평균: 3
 
-
-const numbers = [4, 9, 16, 25];
-const res = numbers.map(function (element, index) {
-  return element * 2;
-});
-
-console.log(`res : ${res}`); //res : 8,18,32,50
-
-const sqt = numbers.map(Math.sqrt); //다른 객체에서 지정해놓은 함수도 사용할 수 있음
-console.log(`sqt : ${sqt}`); //sqt : 2,3,4,5
-
-//reduce()는 하나의 결과값으로 리턴해줌
-function getSum(total, num) {
-  // total: 누적, num:요소
-  return total + num;
-}
-
-const sum = numbers.reduce(getSum);
-console.log(`sum : ${sum}`); //sum : 54
-
-
 */
 let arr = [1, 2, 3, undefined, 4, undefined, undefined, 5];
-const res = arr.map(function (element, index) {
-  return element;
-});
 
-console.log(`res : ${res}`);
-
+console.log(`합계: ${res}`);
+console.log(`평균: ${average}`);
 /*
  [문제2] 
   2차원 배열의 데이터를 이용해서 합계와 평균을 계산하는 프로그램을 구현하세요.
