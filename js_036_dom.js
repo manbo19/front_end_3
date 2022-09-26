fac160dbe2e891cc4925d36a0c274b212667a3dd
jeongmin <jeongmin0619@gmail.com>
2022-09-26 12:29:27 +0900

    20220926

--- js_036_dom.js
+++ js_036_dom.js
@@ -8,3 +8,13 @@ console.log(ptNode);
 //parentNode: [object HTMLDivElement]
 console.log(`parentNode: ${ptNode}`);
 ptNode.style.color = "blue";
+
+//////////////////////////////////////////////////
+//previousSiblig
+let prevNode = myNode.previousSibling;
+//prevNode: [object Text] ->공백을 하나의 텍스트노드로 인식
+console.log(`prevNode: ${prevNode}`);
+
+prevNode = prevNode.previousSibling;
+//prevNode: [object HTMLParagraphElement]
+console.log(`prevNode: ${prevNode}`);
