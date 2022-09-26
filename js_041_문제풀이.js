eb248271444213b0ca78ce51c05e4f7c935ecc8d
jeongmin <jeongmin0619@gmail.com>
2022-09-26 17:57:41 +0900

    20220926

---
+++ js_041_문제풀이.js
@@ -0,0 +1,10 @@
+let date = new Date();
+console.log(date);
+
+let btn = document.getElementsByTagName("button")[0];
+btn.onclick = process;
+
+function process() {
+  let date = new Date();
+  console.log(date);
+}
