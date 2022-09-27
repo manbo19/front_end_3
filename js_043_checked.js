74bf99a62a61d489fc5d276e49e65aa918f22adb
jeongmin <jeongmin0619@gmail.com>
2022-09-27 12:57:49 +0900

    20220927

---
+++ js_043_checked.js
@@ -0,0 +1,27 @@
+let totalCar = document.getElementById("total");
+
+let chbCnt = document.querySelectorAll('tbody input[type="checkbox"]');
+console.log(chbCnt); //NodeList
+
+//NodeList을 Array로 변환
+let myChb = [...chbCnt];
+
+//checkbox에 onclick이벤트가 발생됐을 때 수행될 carCount()를 등록
+myChb.map((element) => {
+  //Window
+  //console.log(this);
+  element.onclick = carCount;
+});
+
+function carCount() {
+  //this는 현재 이벤트가 발생된 checkbox를 나타냄
+  //console.log(this);
+  console.log(this.id);
+
+  /* defaultValue는 <input>에 초기에 입력된 value의 값을 기억해 가져온다. */
+  let basicCar = Number(totalCar.defaultValue);
+
+  if (this.checked) basicCar += Number(this.value);
+
+  totalCar.value = basicCar;
+}
